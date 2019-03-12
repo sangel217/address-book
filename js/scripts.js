b54652073b5175aa3be7b828085f6fcf5011a208
Lela Smith and Sarah Angel <lela@>
2019-03-11 20:38:23 -0700

    create address book

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = []
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+//Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+var newAddressBook = new AddressBook();
+
+$(document).ready(function() {
+  $("form#address-book").submit(function(event) {
+    event.preventDefault();
+    var firstName = $("input#firstName").val();
+    var lastName = $("input#lastName").val();
+    var phoneNumber = $("input#phoneNumber").val();
+    var newContact = new Contact(firstName, lastName, phoneNumber);
+    newAddressBook.addContact(newContact);
+
+
+    $("#result").text(newContact.fullName())
+
+  });
+});
